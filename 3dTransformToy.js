f56a1cf8b7beb48042ecb6f8fcdbc5105bd39367
Haochen Shi <hshi74@wisc.edu>
2020-03-01 22:21:48 -0600

    More examples and clean up the code

--- 3dTransformToy.js
+++ 3dTransformToy.js
@@ -72,11 +72,6 @@ function drawCsys(colors = ["black", "grey"]) {
     }
     
     return csys;
-    
-    // if (drawBlock) {
-    //     context.fillStyle = drawBlock;
-    //     square(context);
-    // }
 }
 
 /**
@@ -87,12 +82,8 @@ function doTransform(scene, transformList, param) {
     // is the text in the code section under the canvas
     let html = "";
 
-    // 0 and 1 are lights, 2 is the coordinate system
-    for (let i = scene.children.length; i >= 3; i--) {
-        // let object = scene.children[i];
-        // object.position.set(0, 0, 0);
-        // object.rotation.set(0, 0, 0);
-        // object.scale.set(1, 1, 1);
+    // 0 - 2 are lights, 3 is the coordinate system
+    for (let i = scene.children.length; i >= 4; i--) {
         scene.remove(scene.children[i]);
     }
 
@@ -148,85 +139,120 @@ function doTransform(scene, transformList, param) {
             }
             html += stylize(amt, `let ` + name + `Mat = new T.MeshStandardMaterial({color: "${color}"});`);
             html += stylize(amt, `let ` + name + `Geom = new T.BoxGeometry(${t[2]}, ${t[3]}, ${t[4]});`);
-            html += stylize(amt, `let ` + name + `Mesh = new T.Mesh(boxGeom, boxMat);`);
+            html += stylize(amt, `let ` + name + `Mesh = new T.Mesh(` + name + `Geom, `+ name + `Mat);`);
             html += stylize(amt, parentName + `.add(` + name + `Mesh);`);
-        } else {
-            // translate, rotate, scale
-            if (command == "translateX") {
-                let x = t[2] * amt;
-                if (object && amt > 0) {
-                    object.translateX(x);
-                }
-                html += stylize(amt, name + `.translationX(${x.toFixed(1)});`);
-            } else if (command == "translateY") {
-                let y = t[2] * amt;
-                if (object && amt > 0) {
-                    object.translateY(y);
-                }
-                html += stylize(amt, name + `.translationY(${y.toFixed(1)});`);
-            } else if (command == "translateZ") {
-                let z = t[2] * amt;
-                if (object && amt > 0) {
-                    object.translateZ(z);
-                }
-                html += stylize(amt, name + `.translationZ(${z.toFixed(1)});`);
-            } else if (command == "position") {
-                let x = t[2] * amt;
-                let y = t[3] * amt;
-                let z = t[4] * amt;
-                if (object && amt > 0) {
-                    object.position.set(x, y, z);
-                }
-                html += stylize(amt, name + `.position.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
-            } else if (command == "rotateX") {
-                let a = t[2] * amt;
-                if (object && amt > 0) {
-                    object.rotateX(degToRad(a));
-                }
-                html += stylize(amt, name + `.rotateX(${a.toFixed(1)});`);
-            } else if (command == "rotateY") {
-                let a = t[2] * amt;
-                if (object && amt > 0) {
-                    object.rotateY(degToRad(a));
-                }
-                html += stylize(amt, name + `.rotateY(${a.toFixed(1)});`);
-            } else if (command == "rotateZ") {
-                let a = t[2] * amt;
-                if (object && amt > 0) {
-                    object.rotateZ(degToRad(a));
-                }
-                html += stylize(amt, name + `.rotateZ(${a.toFixed(1)});`);
-            } else if (command == "rotation") {
-                let x = t[2] * amt;
-                let y = t[3] * amt;
-                let z = t[4] * amt;
-                if (object && amt > 0) {
-                    object.rotation.set(degToRad(x), degToRad(y), degToRad(z));
-                }
-                html += stylize(amt, name + `.rotation.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
-            } else if (command == "rotateAxis") {
-                let x = t[2];
-                let y = t[3];
-                let z = t[4];
-                let a = t[5] * amt;
-                let axis = new T.Vector3(x, y, z);
-                if (object && amt > 0) {
-                    object.rotateOnAxis(axis, degToRad(a));
-                }
-                html += stylize(amt, name + `.rotateOnAxis(new T.Vector3(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)}), ${a.toFixed(1)});`);
-            } else if (command == "scale") {
-                let x = amt * t[2] + (1 - amt) * 1;
-                let y = amt * t[3] + (1 - amt) * 1;
-                let z = amt * t[4] + (1 - amt) * 1;
-                if (object && amt > 0) {
-                    object.scale.set(x, y, z);
-                }
-                html += stylize(amt, name + `.scale.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
-            } else { // bad command
-                console.log(`Bad transform ${t}`);
+        } else if (command == "cone") {
+            let color = t.length > 4 ? t[4] : "blue";
+            let parent;
+            let parentName;
+            if (t.length > 5) {
+                parent = scene.getObjectByName(t[5]);
+                parentName = t[5];
+            } else {
+                parent = scene;
+                parentName = "scene";
             }
+            if (!object && amt) {
+                let coneMat = new T.MeshStandardMaterial({color: color});
+                let radius = t[2];
+                let height = t[3];
+                let coneGeom = new T.ConeGeometry(radius, height);
+                let coneMesh = new T.Mesh(coneGeom, coneMat);
+                coneMesh.name = name;
+                parent.add(coneMesh);
+                let axesHelper = new T.AxesHelper(3);
+                coneMesh.add(axesHelper);
+            } 
+            if (object && !amt) {
+                scene.remove(object);
+            }
+            html += stylize(amt, `let ` + name + `Mat = new T.MeshStandardMaterial({color: "${color}"});`);
+            html += stylize(amt, `let ` + name + `Geom = new T.ConeGeometry(${t[2]}, ${t[3]});`);
+            html += stylize(amt, `let ` + name + `Mesh = new T.Mesh(` + name + `Geom, `+ name + `Mat);`);
+            html += stylize(amt, parentName + `.add(` + name + `Mesh);`);
+        } else if (command == "translateX") {
+            let x = t[2] * amt;
+            if (object && amt > 0) {
+                object.translateX(x);
+            }
+            html += stylize(amt, name + `.translationX(${x.toFixed(1)});`);
+        } else if (command == "translateY") {
+            let y = t[2] * amt;
+            if (object && amt > 0) {
+                object.translateY(y);
+            }
+            html += stylize(amt, name + `.translationY(${y.toFixed(1)});`);
+        } else if (command == "translateZ") {
+            let z = t[2] * amt;
+            if (object && amt > 0) {
+                object.translateZ(z);
+            }
+            html += stylize(amt, name + `.translationZ(${z.toFixed(1)});`);
+        } else if (command == "position") {
+            let x = t[2] * amt;
+            let y = t[3] * amt;
+            let z = t[4] * amt;
+            if (object && amt > 0) {
+                object.position.set(x, y, z);
+            }
+            html += stylize(amt, name + `.position.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
+        } else if (command == "rotateX") {
+            let a = t[2] * amt;
+            if (object && amt > 0) {
+                object.rotateX(degToRad(a));
+            }
+            html += stylize(amt, name + `.rotateX(${a.toFixed(1)});`);
+        } else if (command == "rotateY") {
+            let a = t[2] * amt;
+            if (object && amt > 0) {
+                object.rotateY(degToRad(a));
+            }
+            html += stylize(amt, name + `.rotateY(${a.toFixed(1)});`);
+        } else if (command == "rotateZ") {
+            let a = t[2] * amt;
+            if (object && amt > 0) {
+                object.rotateZ(degToRad(a));
+            }
+            html += stylize(amt, name + `.rotateZ(${a.toFixed(1)});`);
+        } else if (command == "rotation") {
+            let x = t[2] * amt;
+            let y = t[3] * amt;
+            let z = t[4] * amt;
+            if (object && amt > 0) {
+                object.rotation.set(degToRad(x), degToRad(y), degToRad(z));
+            }
+            html += stylize(amt, name + `.rotation.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
+        } else if (command == "rotateOnAxis") {
+            let x = t[2];
+            let y = t[3];
+            let z = t[4];
+            let a = t[5] * amt;
+            let axis = new T.Vector3(x, y, z);
+            if (object && amt > 0) {
+                object.rotateOnAxis(axis, degToRad(a));
+            }
+            html += stylize(amt, name + `.rotateOnAxis(new T.Vector3(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)}), ${a.toFixed(1)});`);
+        } else if (command == "lookAt") {
+            let x = t[2];
+            let y = t[3];
+            let z = t[4];
+            if (object && amt > 0) {
+                object.lookAt(x, y, z);
+            }
+            html += stylize(amt, name + `.lookAt(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
+        } else if (command == "scale") {
+            let x = amt * t[2] + (1 - amt) * 1;
+            let y = amt * t[3] + (1 - amt) * 1;
+            let z = amt * t[4] + (1 - amt) * 1;
+            if (object && amt > 0) {
+                object.scale.set(x, y, z);
+            }
+            html += stylize(amt, name + `.scale.set(${x.toFixed(1)}, ${y.toFixed(1)}, ${z.toFixed(1)});`);
+        } else { // bad command
+            console.log(`Bad transform ${t}`);
         }
     });
+
     return html;
 }
 
@@ -248,7 +274,7 @@ export function setExample(title, transforms = undefined) {
     // left part, including a canvas, checkboxes, a slider, and a code block
     let leftDiv = document.createElement("div");
     leftDiv.id = name + "-leftDiv";
-    leftDiv.style.cssText = "width: 600px";
+    leftDiv.style.cssText = "width: 600px;";
     document.getElementById(exampleDiv.id).appendChild(leftDiv);
 
     let leftRenderer = new T.WebGLRenderer();
@@ -275,9 +301,9 @@ export function setExample(title, transforms = undefined) {
     leftDirLight.position.set(1, 1, 0);
     leftScene.add(leftDirLight);
 
-    // let dirLight2 = new T.DirectionalLight(0xD0E0F0, 0.7);
-    // dirLight2.position.set(0, 1, -0.2);
-    // scene.add(dirLight2);
+    let leftDirLight2 = new T.DirectionalLight(0xD0E0F0, 0.7);
+    leftDirLight2.position.set(0, 1, -0.2);
+    leftScene.add(leftDirLight2);
 
     let leftCsys = drawCsys();
     leftScene.add(leftCsys);
@@ -333,21 +359,21 @@ export function setExample(title, transforms = undefined) {
     rightDirLight.position.set(1, 1, 0);
     rightScene.add(rightDirLight);
 
-    // let dirLight2 = new T.DirectionalLight(0xD0E0F0, 0.7);
-    // dirLight2.position.set(0, 1, -0.2);
-    // scene.add(dirLight2);
+    let rightDirLight2 = new T.DirectionalLight(0xD0E0F0, 0.7);
+    rightDirLight2.position.set(0, 1, -0.2);
+    rightScene.add(rightDirLight2);
 
     let rightCsys = drawCsys();
     rightScene.add(rightCsys);
 
     let rightPanel = document.createElement("div");
     rightPanel.id = name + "-rightPanel";
-    rightPanel.style.cssText = "margin-top: 20px";
+    rightPanel.style.cssText = "margin-top: 63px";
     document.getElementById(rightDiv.id).appendChild(rightPanel);
 
     let rightCodeDiv = document.createElement("div");
     rightCodeDiv.style.cssText = "font-family: 'Courier New', Courier, monospace; " +
-        "font-size: 100%; padding-top: 35px";
+        "font-size: 100%;";
     document.getElementById(rightDiv.id).appendChild(rightCodeDiv);
 
     resultTog.onchange = function () {
